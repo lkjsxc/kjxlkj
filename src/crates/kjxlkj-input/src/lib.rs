c5c5f086c1873aed46e19b94a5f923bcd7b8204f
lkjsxc <lkjsxc@gmail.com>
2026-02-03 02:41:34 +0900

    feat(input): add user-defined mappings

--- src/crates/kjxlkj-input/src/lib.rs
+++ src/crates/kjxlkj-input/src/lib.rs
@@ -7,6 +7,7 @@ mod digraph;
 mod key;
 mod mapping;
 mod parser;
+mod user_mapping;
 mod validation;
 
 pub use cmdline::CommandLine;
@@ -14,6 +15,7 @@ pub use digraph::{Digraph, DigraphTable};
 pub use key::{Key, KeyCodeWrapper, KeySequence, Modifiers};
 pub use mapping::KeyMap;
 pub use parser::InputParser;
+pub use user_mapping::{UserMapFlags, UserMapMode, UserMapStore, UserMapping};
 pub use validation::{
     validate_buffer_name, validate_column, validate_count, validate_file_path,
     validate_line_number, validate_mark, validate_pattern, validate_register, validate_split_ratio,
